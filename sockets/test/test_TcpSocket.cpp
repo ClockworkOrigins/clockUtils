ac68ea31d885e0080805f259581083cc4184ac5d
Daniel Bonrath <Big-DB@web.de>
2014-04-04 13:06:14 +0200

    CU-5 started tests

--- sockets/test/test_TcpSocket.cpp
+++ sockets/test/test_TcpSocket.cpp
@@ -1,8 +1,39 @@
-//#include "clockUtils/sockets/TcpSocket.h"
+#include "clockUtils/sockets/TcpSocket.h"
 
 #include "gtest/gtest.h"
 
+using namespace clockUtils;
+using namespace clockUtils::sockets;
+
+void connectionAccepted(const TcpSocket &) {
+
+}
+
 TEST(TcpSocket, connect) { // tests connect with all possible errors
+	TcpSocket ts;
+	ClockError e = ts.connect("1", 12345, 1000);
+
+	EXPECT_EQ(ClockError::INVALID_IP, e);
+	
+	e = ts.connect("127.0.0.1", 0, 1000);
+
+	EXPECT_EQ(ClockError::INVALID_PORT, e);
+
+	e = ts.connect("192.168.255.255", 12345, 1000);
+
+	EXPECT_EQ(ClockError::TIMEOUT, e);
+
+	ts.connect("127.0.0.1", 12345, 1000);
+
+	EXPECT_EQ(ClockError::CONNECTION_FAILED, e);
+
+	TcpSocket server;
+
+	e = server.listen(12345, 1, false, std::bind(connectionAccepted, std::placeholders::_1));
+
+	e = ts.connect("127.0.0.1", 12345, 1000);
+
+	EXPECT_EQ(ClockError::SUCCESS, e);
 }
 
 TEST(TcpSocket, listen) { // tests incoming connections: one thread listening on a port and five or something like that joining
@@ -25,10 +56,8 @@ TEST(TcpSocket, useUnready) {
 	int a;
 
 	TcpSocket sock1;
-	EXPECT_EQ(ClockErros::NOT_READY, sock1.writePacket(buffer));
-	EXPECT_EQ(ClockErros::NOT_READY, sock1.writePacket(static_cast<char *>(&buffer[0]), buffer.size()));
-	EXPECT_EQ(ClockErros::NOT_READY, sock1.read(buffer));
-	EXPECT_EQ(ClockErros::NOT_READY, sock1.receivePacket(buffer));
+	/*EXPECT_EQ(ClockError::NOT_READY, sock1.writePacket(buffer));
+	EXPECT_EQ(ClockError::NOT_READY, sock1.writePacket(static_cast<char *>(&buffer[0]), buffer.size()));
+	EXPECT_EQ(ClockError::NOT_READY, sock1.read(buffer));
+	EXPECT_EQ(ClockError::NOT_READY, sock1.receivePacket(buffer));*/
 }
-
-s
