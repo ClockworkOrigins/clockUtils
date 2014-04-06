0fe64f7a37aa076f31b4d08cc96b19296f0469ce
Daniel Bonrath <Big-DB@web.de>
2014-04-06 21:04:13 +0200

    CU-7 refactored errors.h

--- include/clockUtils/errors.h
+++ include/clockUtils/errors.h
@@ -1,27 +1,27 @@
-#ifndef __CLOCKERRORS_H__
-#define __CLOCKERRORS_H__
+#ifndef __CLOCKUTILS_CLOCKERRORS_H__
+#define __CLOCKUTILS_CLOCKERRORS_H__
 
 namespace clockUtils {
 
-enum class ClockError {
-	SUCCESS,
-	// general
-	NOT_READY,
-	TIMEOUT,
-	INVALID_ARGUMENT,
-	INVALID_USAGE,
-	IN_PROGRESS,
-	// sockets
-	INVALID_IP,
-	INVALID_PORT,
-	ADDRESS_INUSE,
-	CONNECTION_FAILED,
-	NOT_CONNECTED,
-	//
-	UNKNOWN,
-	ERROR_COUNT
-};
+	enum class ClockError {
+		SUCCESS,
+		// general
+		NOT_READY,
+		TIMEOUT,
+		INVALID_ARGUMENT,
+		INVALID_USAGE,
+		IN_PROGRESS,
+		// sockets
+		INVALID_IP,
+		INVALID_PORT,
+		ADDRESS_INUSE,
+		CONNECTION_FAILED,
+		NOT_CONNECTED,
+		//
+		UNKNOWN,
+		ERROR_COUNT
+	};
 
 } /* namespace clockUtils */
 
-#endif /* __CLOCKERRORS_H__ */
+#endif /* __CLOCKUTILS_CLOCKERRORS_H__ */
