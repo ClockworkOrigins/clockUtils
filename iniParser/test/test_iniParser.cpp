1bfeeb5a9452c950ce4554685e48a3082d8ca40c
Daniel Bonrath <Big-DB@web.de>
2014-10-17 23:25:13 +0200

    CU-19 (iniParser) fixed iniParser test

--- iniParser/test/test_iniParser.cpp
+++ iniParser/test/test_iniParser.cpp
@@ -12,7 +12,6 @@ TEST(IniParser, loadInvalidFile) {
 	IniParser i1;
 	EXPECT_EQ(ClockError::FILENOTFOUND, i1.load("NotExistentFile.ini"));
 	EXPECT_EQ(ClockError::WRONG_SYNTAX, i1.load("resources/DefectSection.ini"));
-	EXPECT_EQ(ClockError::WRONG_SYNTAX, i1.load("resources/NoSection.ini"));
 	EXPECT_EQ(ClockError::WRONG_SYNTAX, i1.load("resources/DefectField.ini"));
 }
 
