1f9bd52a7e013fb92d0a05ede7fdb1f21fa0152d
Daniel Bonrath <Big-DB@web.de>
2014-11-19 12:23:38 +0100

    CU-9 (sockets) fixed UdpSocket for linux

--- sockets/src/linux/UdpSocket.cpp
+++ sockets/src/linux/UdpSocket.cpp
@@ -1,61 +1,33 @@
-#include "clockUtils/sockets/TcpSocket.h"
+#include "clockUtils/sockets/UdpSocket.h"
 
 #ifndef _GLIBCXX_USE_NANOSLEEP
 	#define _GLIBCXX_USE_NANOSLEEP // needed for sleep_for, see http://stackoverflow.com/questions/4438084/stdthis-threadsleep-for-and-gcc, but is fixed in newer version of gcc (>4.8 i think)
 #endif
 
-#if CLOCKUTILS_PLATFORM == CLOCKUTILS_PLATFORM_LINUX
-	#include <sys/socket.h>
-	#include <unistd.h>
-#endif
-
 #include <chrono>
+#include <sys/socket.h>
 #include <thread>
+#include <unistd.h>
 
 #include "clockUtils/errors.h"
 
 namespace clockUtils {
 namespace sockets {
 
-	TcpSocket::TcpSocket() : _sock(-1), _status(SocketStatus::INACTIVE), _todoLock(), _todo(), _buffer(), _terminate(false), _worker(nullptr), _objCondExecutable(), _objCondMut(), _objCondUniqLock(_objCondMut) {
-		_worker = new std::thread([this]() {
-				while (!_terminate) {
-					_todoLock.lock();
-					while(_todo.size() > 0) {
-						std::vector<uint8_t> tmp = std::move(_todo.front());
-						_todoLock.unlock();
-
-						writePacket(const_cast<const unsigned char *>(&tmp[0]), tmp.size());
-
-						_todoLock.lock();
-						_todo.pop();
-					}
-					_todoLock.unlock();
-					_objCondExecutable.wait(_objCondUniqLock);
-				}
-			});
+	UdpSocket::UdpSocket() : _sock(-1), _buffer(), _terminate(false) {
 	}
 
-	TcpSocket::~TcpSocket() {
-		_terminate = true;
-		_objCondExecutable.notify_all();
-		_worker->join();
-		delete _worker;
+	UdpSocket::~UdpSocket() {
 		close();
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 
-	void TcpSocket::close() {
-		if (_status != SocketStatus::INACTIVE) {
-			// needed to stop pending accept operations
-			::shutdown(_sock, SHUT_RDWR); // FIXME: only do this if connected?, check for errorcode than
-			if (-1 == ::close(_sock)) perror("close");
-			_sock = -1;
-			_status = SocketStatus::INACTIVE;
-		}
+	void UdpSocket::close() {
+		::close(_sock); // FIXME: only do this if connected?, check for errorcode than
+		if (-1 == ::close(_sock)) perror("close");
+		_sock = -1;
 	}
 
-	ClockError TcpSocket::getLastError() {
+	ClockError UdpSocket::getLastError() {
 		if (errno == EADDRINUSE) {
 			return ClockError::ADDRESS_INUSE;
 		} else if (errno == EINTR) {
