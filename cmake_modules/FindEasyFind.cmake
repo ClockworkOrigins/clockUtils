2df648e4d792b34b45efe31ec67bd880a52b495e
Daniel Bonrath <Big-DB@web.de>
2015-06-17 21:04:11 +0200

    CU-38 added license and build script for windows source and binary package

--- cmake_modules/FindEasyFind.cmake
+++ cmake_modules/FindEasyFind.cmake
@@ -1,3 +1,21 @@
+#
+# clockUtils
+# Copyright (2015) Michael Baer, Daniel Bonrath, All rights reserved.
+#
+# This file is part of clockUtils; clockUtils is free software; you can
+# redistribute it and/or modify it under the terms of the GNU Lesser
+# General Public License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.
+#
+
 # easyFind version 1.0 - 25.12.2012 - # 009
 
 # Find $LIBNAME includes and library
--- sockets/src/linux/TcpSocket.cpp
+++ sockets/src/linux/TcpSocket.cpp
@@ -1,3 +1,21 @@
+/**
+ * clockUtils
+ * Copyright (2015) Michael Baer, Daniel Bonrath, All rights reserved.
+ *
+ * This file is part of clockUtils; clockUtils is free software; you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
 #include "clockUtils/sockets/TcpSocket.h"
 
 #ifndef _GLIBCXX_USE_NANOSLEEP
