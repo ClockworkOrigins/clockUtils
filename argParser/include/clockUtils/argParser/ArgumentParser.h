8b3cce61af930909e41388c1f8477f51891b918e
Daniel Bonrath <Big-DB@web.de>
2015-06-18 15:19:28 +0200

    CU-38 changes license to LPGL2.1 and renamed libraries to show major version

--- argParser/include/clockUtils/argParser/ArgumentParser.h
+++ argParser/include/clockUtils/argParser/ArgumentParser.h
@@ -2,10 +2,10 @@
  * clockUtils
  * Copyright (2015) Michael Baer, Daniel Bonrath, All rights reserved.
  *
- * This file is part of clockUtils; clockUtils is free software; you can
- * redistribute it and/or modify it under the terms of the GNU Lesser
- * General Public License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
+ * This file is part of clockUtils; clockUtils is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,7 +13,8 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #ifndef __CLOCKUTILS_ARGPARSER_ARGUMENTPARSER_H__
