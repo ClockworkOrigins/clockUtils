2569f70949beb9a38a10edd737218763e881a99b
Daniel Bonrath <bonne@clockwork-origins.de>
2016-12-03 10:54:09 +0100

    CU-142 fixed a linux warning

--- include/clockUtils/log/Logger.h
+++ include/clockUtils/log/Logger.h
@@ -40,13 +40,13 @@ namespace log {
 namespace detail {
 	template<typename S, typename T>
 	class isStreamable {
+	public:
 		template<typename SS, typename TT>
 		static auto test(int) -> decltype(std::declval<SS &>() << std::declval<TT>(), std::true_type());
 
 		template<typename, typename>
 		static auto test(...) -> std::false_type;
 
-	public:
 		static const bool value = decltype(test<S, T>(0))::value;
 	};
 } /* namespace detail */
